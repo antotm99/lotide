6c895a9f9892e6aeac6533c3d5ca2d020a1e1b04
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-05 22:14:00 +0000

    assertArraysEquals.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (arrayOne !== arrayTwo) {
+    console.log("Assertion Failed: " + [arrayOne] + " !== " + [arrayTwo]);
+    return "Assertion Failed: " + [arrayOne] + " !== " + [arrayTwo];
+  }else if (arrayOne === ArrayTwo) {
+    console.log("Assertion Passed: " + [arrayOne] + " === " + [arrayTwo]);
+    return "Assertion Passed: " + [arrayOne] + " === " + [arrayTwo];
+  }
+}
