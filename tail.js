f3d1082264a90c90375b2d27a82db32c3d63a478
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-11 18:10:43 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,22 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
-    return "Assertion Failed: " + [actual] + " !== " + [expected];
-  } else if (actual === expected) {
-    console.log("Assertion Passed: " + [actual] + " === " + [expected]);
-    return "Assertion Passed: " + [actual] + " === " + [expected];
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const tail = function(array) {
   if (array.length > 1) {
     return array.shift();
   }
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
+module.exports = tail;
