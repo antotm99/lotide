842b33a62316dc9af64856a23c28995cd456d1d6
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-11 18:13:08 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
-    return "Assertion Failed: " + [actual] + " !== " + [expected];
-  } else if (actual === expected) {
-    console.log("Assertion Passed: " + [actual] + " === " + [expected]);
-    return "Assertion Passed: " + [actual] + " === " + [expected];
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const eqArrays = function (arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
@@ -24,4 +10,4 @@ const eqArrays = function (arrayOne, arrayTwo) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
