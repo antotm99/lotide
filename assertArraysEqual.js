4809c9e59d39a31efc8bd227c705bd3ea3576b46
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-11 18:17:09 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,3 @@
-const eqArrays = function (arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne !== arrayTwo)) {
     console.log("Assertion Failed: " + [arrayOne] + " !== " + [arrayTwo]);
@@ -19,3 +7,5 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
     return "Assertion Passed: " + [arrayOne] + " === " + [arrayTwo];
   }
 }
+
+module.exports = assertArraysEqual;
