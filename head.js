42a4158c66ec35660ced62b2a5b04bfbca67efd5
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-11 17:47:57 +0000

    refactored

--- head.js
+++ head.js
@@ -1,16 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
-    return "Assertion Failed: " + [actual] + " !== " + [expected];
-  } else if (actual === expected) {
-    console.log("Assertion Passed: " + [actual] + " === " + [expected]);
-    return "Assertion Passed: " + [actual] + " === " + [expected];
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   if (array === null){
@@ -20,6 +8,5 @@ const head = function(array) {
   } 
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
 
